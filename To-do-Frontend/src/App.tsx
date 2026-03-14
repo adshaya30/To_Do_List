9848ca94f9854f927653a509994100397134dd82
adshaya30 <yaliads30@gmail.com>
2026-03-14 10:28:57 +0530

    Frontend setup React Typescript+Tailwincss

---
+++ To-do-Frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+const App=()=>{
+
+  return (
+    <>
+      <div className="text-3xl font-bold underline text-blue-600">
+       Hello World
+      </div>
+    </>
+  )
+}
+
+export default App;
